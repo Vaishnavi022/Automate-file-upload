9c8727a41b82a06f840833263a2fe174adb8d016
Chaudhari Vaishnavi Narendra <138233760+Vaishnavi022@users.noreply.github.com>
2026-04-20 15:34:21 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -2,4 +2,8 @@ import boto3
 
 s3 = boto3.client('s3')
 
-s3.upload_file('index.html', 'your-bucket-name', 'index.html')
+bucket_name = "associate-file-upload"
+
+s3.upload_file('index.html', bucket_name, 'index.html')
+
+print("✅ File uploaded successfully!")
